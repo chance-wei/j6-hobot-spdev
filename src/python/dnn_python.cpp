9a6d9a715ded1f61fa818191ebe0f5d8d4f1f020
fuhua-wang <fuhua.wang@horizon.cc>
2024-09-30 16:54:10 +0800

    fix(dnn):[RDK-307](x5-hobot-spdev)Solve the CenterNet demo segment fault.
    Summary:
    - Incorrect memory allocation type. Causing a segment fault.
    
    Signed-off-by: fuhua.wang <fuhua.wang@horizon.cc>

--- src/python/dnn_python.cpp
+++ src/python/dnn_python.cpp
@@ -92,6 +92,8 @@ PyObject* buffer_2_pyarray(void *arr, hbDNNTensorProperties &properties) {
     switch (dataType) {
         case HB_DNN_TENSOR_TYPE_S8:
             return buffer_2_numpy(reinterpret_cast<int8_t *>(arr), properties, NPY_INT8, sizeof(int8_t));
+        case HB_DNN_TENSOR_TYPE_S16:
+            return buffer_2_numpy(reinterpret_cast<int16_t *>(arr), properties, NPY_INT32, sizeof(int16_t));
         case HB_DNN_TENSOR_TYPE_S32:
             return buffer_2_numpy(reinterpret_cast<int32_t *>(arr), properties, NPY_INT32, sizeof(int32_t));
         case HB_DNN_TENSOR_TYPE_F32:
